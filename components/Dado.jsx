07bd83050673a802707262e4efd14989f9befb09
aurealubambo9-beep <aurealubambo9@gmail.com>
2026-09-14 20:32:24 -0300

    Add Dado component to display dice values

---
+++ components/Dado.jsx
@@ -0,0 +1,22 @@
+import styles from "./Dado.module.css";
+
+export default function Dado({ valor }) {
+  if (!valor) {
+    return (
+      <div className={`${styles.dado} ${styles.vazio}`}>
+        <span>?</span>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.dado}>
+      <img
+        src={`/dice/${valor}.png`.replace(".png", ".svg")}
+        alt={`Dado com valor ${valor}`}
+        width={70}
+        height={70}
+      />
+    </div>
+  );
+}
